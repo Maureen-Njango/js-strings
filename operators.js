b5b162bf1e6070287ffcec52ee310fe07b519dd4
Maueen-Njango <maureennjangombira@gmail.com>
2024-03-07 11:38:57 +0300

    changes

--- operators.js
+++ operators.js
@@ -1,24 +1,30 @@
 let a = "ectravaganza";
 console.log(a.slice(-4));
+//2
+
+let originstring="The quick fox jumped over the lazy dog";
+let stringToAdd="eat";
+let indexPosition=4;
+newString=originstring.slice(0,indexPosition)+ stringToAdd+originstring.slice(indexPosition);
+console.log(newString);
 
 let m = "The quick  brown fox jumped over the lazy dog"
 let n = "the";
 let v = "brown";
 console.log((m.match(/the/)).length);
 console.log((m.match(/brown/)).length);
-
+//3
 let z = "the pupil are reading in the library"
 let o = "are"
 let y = "The child was sitting on the table before it fell"
 let f = "sitting"
 console.log(z.includes(o));
 console.log(y.includes(f));
-
+//4
 let b = "wonderful"
 console.log(b.toUpperCase());
 let c = "amaizing"
 console.log(c.toLowerCase());
 let x = "underneath"
 console.log(x.toLowerCase());
-let w = "A wonderful World"
-console.log(w.Titlecase());
+
