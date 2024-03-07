0180e0235a7558b5daa4d1a60f8146261c689c4e
Maueen-Njango <maureennjangombira@gmail.com>
2024-03-07 08:11:08 +0300

    add file

---
+++ operators.js
@@ -0,0 +1,24 @@
+let a = "ectravaganza";
+console.log(a.slice(-4));
+
+let m = "The quick  brown fox jumped over the lazy dog"
+let n = "the";
+let v = "brown";
+console.log((m.match(/the/)).length);
+console.log((m.match(/brown/)).length);
+
+let z = "the pupil are reading in the library"
+let o = "are"
+let y = "The child was sitting on the table before it fell"
+let f = "sitting"
+console.log(z.includes(o));
+console.log(y.includes(f));
+
+let b = "wonderful"
+console.log(b.toUpperCase());
+let c = "amaizing"
+console.log(c.toLowerCase());
+let x = "underneath"
+console.log(x.toLowerCase());
+let w = "A wonderful World"
+console.log(w.Titlecase());
